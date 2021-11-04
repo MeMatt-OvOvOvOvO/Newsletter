bf7e228eb93c8ac01c4e960e40effdc748424630
Mateusz Janicki <mj4nicki@gmail.com>
2021-11-04 19:19:48 +0100

    done

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express")
 var app = express()
-var PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
+const PORT = 4000;
 
 const bodyParser = require("body-parser")
 app.use(express.static('static'))
@@ -19,46 +19,41 @@ app.get("/", function (req, res) {
 })
 
 app.get("/removeUserBySelect", function(req, res){
-    res.send(`
-    <body>
-        <div>
-            <form action="/handleForm" method="POST">
-                <select name="email" id="email">
-                    <option value="asfadf">${users.map(e=>e.email)}</option>
-                </select>
-                <input type="button" value="usun">
-            </form>
-        </div>
-    </body>`)   
+    var select = "<form action='/delete' method='POST'><select name='email'>"
+    for(i = 0; i < users.length; i++){
+        select = select + "<option value='" + users[i].email + "'>" + users[i].email + "</option>"
+    }
+    all = select + "</select><input type='submit' value='usun'></form>"
+    res.send(all) 
     console.log(users[1])
 })
 
 app.get("/removeUserByRadio", function(req, res){
-    res.send(`
-    <body>
-        <div>
-            <form action="/handleForm" method="POST">
-                <input type="radio" id="email" name="email" value="${users.email}">
-                <label for="${users.email}">${users.email}</label><br>
-                <input type="button" value="usun">
-            </form>
-        </div>
-    </body>`)   
+    var buttons = "<form action='/delete' method='POST'>"
+    for(i = 0; i < users.length; i++){
+        buttons = buttons + "<input type='radio' name='email' value='" + users[i].email + "'>" + users[i].email + "<br>"
+    }
+    all = buttons + "</select><input type='submit' value='usun'></form>"
+    res.send(all)  
     console.log(req.users)
 })
 
 app.get("/removeUserByCheckbox", function(req, res){
-    res.send(`
-    <body>
-        <div>
-            <form action="/handleForm" method="POST">
-                <input type="checkbox" id="email" name="email" value="${users.email}">
-                <label for="${users.email}">${users.email}</label><br>
-                <input type="button" value="usun">
-            </form>
-        </div>
-    </body>`)   
+    var buttons = "<form action='/delete' method='POST'>"
+    for(i = 0; i < users.length; i++){
+        buttons = buttons + "<input type='checkbox' name='email' value='" + users[i].email + "'>" + users[i].email + "<br>"
+    }
+    all = buttons + "</select><input type='submit' value='usun'></form>"
+    res.send(all)  
     console.log(req.users)
+})
+app.post("/delete", function(req, res){
+    for(i = 0; i < users.length; i++){
+        if(req.body.email == users[i].email){
+            users.splice(i, 1);
+        }
+    }
+    res.redirect("/");
 })
 app.post("/handleForm", function (req, res) {
     for (const x in users){
@@ -77,5 +72,5 @@ app.post("/handleForm", function (req, res) {
 })
 
 app.listen(PORT, function () {
-    console.log("start serwera na porcie " + PORT)
+    console.log("start serwera na porcie " + PORT )
 })
